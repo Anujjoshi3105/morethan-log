4f4ea857f55b8c2d7528353ff29e05487e573ed3
Anuj Joshi <142736390+Anujjoshi3105@users.noreply.github.com>
2025-05-25 13:23:00 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -31,7 +31,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.NOTION_PAGE_ID,
+    pageId: 1fe5c6ea762e8033a40af275522e31e8,
   },
 
   // plugin configuration (optional)
